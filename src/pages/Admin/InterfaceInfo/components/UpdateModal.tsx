d330e3b392107e6df4a8e026e295b48c2b40f03a
zd-glucacxe <glucacxe@gmail.com>
2023-08-09 14:02:03 +0800

    Initial commit

---
+++ src/pages/Admin/InterfaceInfo/components/UpdateModal.tsx
@@ -0,0 +1,34 @@
+ import {
+  ProColumns, ProTable,
+} from '@ant-design/pro-components';
+import { Modal } from 'antd';
+import React from 'react';
+
+export type Props = {
+  values: API.InterfaceInfo;
+  columns: ProColumns<API.InterfaceInfo>[];
+  onCancel: () => void;
+  onSubmit: (values: API.InterfaceInfo) => Promise<void>;
+  visible: boolean;
+};
+
+const UpdateModal: React.FC<Props> = React.memo((props) => {
+  const { values, visible, columns, onCancel, onSubmit } = props;
+
+  return (
+    <Modal visible={visible} footer={null} onCancel={() => onCancel?.()}>
+      <ProTable
+        type="form"
+        columns={columns}
+        form={{
+          initialValues: values
+        }}
+        onSubmit={async (value) => {
+          onSubmit?.(value);
+        }}
+      />
+    </Modal>
+  );
+});
+
+export default UpdateModal;
